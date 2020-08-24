ef4e81239b3ee5b263982c1b723884e2b27630cf
Dav1d <dav1dch@outlook.com>
2020-08-25 00:19:10 +0800

    add contextWork api

--- src/main/java/com/gowithu/springboot/controller/WorkController.java
+++ src/main/java/com/gowithu/springboot/controller/WorkController.java
@@ -1,13 +1,11 @@
 package com.gowithu.springboot.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import com.gowithu.springboot.dao.AnswerTemplate;
 import com.gowithu.springboot.dao.WorkTemplate;
 import com.gowithu.springboot.entity.Answer;
 import com.gowithu.springboot.entity.Work;
-
+import java.util.List;
+import java.util.Map;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,72 +18,94 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class WorkController {
-    @Autowired private WorkTemplate workTemplate;
-    @Autowired private AnswerTemplate answerTemplate;
+  @Autowired
+  private WorkTemplate workTemplate;
 
-    @ResponseBody
-    @PostMapping("/getStudentWork")
-    public List<Work> getStudentWork(@RequestBody Map<String, Object> data){
-        String studentOpenId = data.get("studentOpenId").toString();
-        return workTemplate.findByStudentOpenId(studentOpenId);
+  @Autowired
+  private AnswerTemplate answerTemplate;
+
+  @ResponseBody
+  @PostMapping("/getStudentWork")
+  public List<Work> getStudentWork(@RequestBody Map<String, Object> data) {
+    String studentOpenId = data.get("studentOpenId").toString();
+    return workTemplate.findByStudentOpenId(studentOpenId);
+  }
+
+  @ResponseBody
+  @PostMapping("/getTeacherWork")
+  public List<Work> getTeacherWork(@RequestBody Map<String, Object> data) {
+    String teacherOpenId = data.get("teacherOpenId").toString();
+    return workTemplate.findByTeacherOpenId(teacherOpenId);
+  }
+
+  @ResponseBody
+  @PostMapping("/addWork")
+  public String addWork(@RequestBody Map<String, Object> data) {
+    Work work = new Work();
+    work.setAnswer((List<String>) data.get("answer"));
+    work.setDeclaration(data.get("declaration").toString());
+    work.setWorkName(data.get("workName").toString());
+    work.setTeacherOpenId(data.get("teacherOpenId").toString());
+    work.setStudentOpenId(data.get("studentOpenId").toString());
+    work.setCount((Integer.parseInt(data.get("count").toString())));
+    work.setSelection((List<String[]>) data.get("selection"));
+    work.setTitle((List<String>) data.get("title"));
+    work.setType(0);
+    work.setScore(0);
+    workTemplate.save(work);
+    return "success";
+  }
+
+  @ResponseBody
+  @PostMapping("/addContextWork")
+  public String addContextWork(@RequestBody Map<String, Object> data) {
+    Work work = new Work();
+    work.setDeclaration(data.get("declaration").toString());
+    work.setWorkName(data.get("workName").toString());
+    work.setTeacherOpenId(data.get("teacherOpenId").toString());
+    work.setStudentOpenId(data.get("studentOpenId").toString());
+    work.setContext(data.get("context").toString());
+    work.setType(1);
+    work.setScore(0);
+    workTemplate.save(work);
+    return "success";
+  }
+
+  @ResponseBody
+  @PostMapping("/getWork")
+  public Work getWork(@RequestBody Map<String, Object> data) {
+    return workTemplate.findById(new ObjectId(data.get("id").toString()));
+  }
+
+  @ResponseBody
+  @PostMapping("/getAnswer")
+  public Answer getAnswer(@RequestBody Map<String, Object> data) {
+    return answerTemplate.findByOpenIdAndWorkId(
+      data.get("openId").toString(),
+      new ObjectId(data.get("workId").toString())
+    );
+  }
+
+  @ResponseBody
+  @PostMapping("/addAnswer")
+  public String addAnswer(@RequestBody Map<String, Object> data) {
+    //TODO: process POST request
+    Answer answer = new Answer();
+    List<String> answerList = (List<String>) data.get("answerList");
+    answer.setAnswer(answerList);
+    answer.setWorkId(new ObjectId(data.get("workId").toString()));
+    answer.setStudentId(data.get("studentOpenId").toString());
+    answerTemplate.saveAnswer(answer);
+    Work work = workTemplate.findById(
+      new ObjectId(data.get("workId").toString())
+    );
+    int tmp = 0;
+    for (int i = 0; i < work.getAnswer().size(); i++) {
+      if (work.getAnswer().get(i).equals(answerList.get(i))) tmp++;
     }
-
-    @ResponseBody
-    @PostMapping("/getTeacherWork")
-    public List<Work> getTeacherWork(@RequestBody Map<String, Object> data){
-        String teacherOpenId = data.get("teacherOpenId").toString();
-        return workTemplate.findByTeacherOpenId(teacherOpenId);
-    }
-
-    @ResponseBody
-    @PostMapping("/addWork")
-    public String addWork(@RequestBody Map<String, Object> data){
-        Work work = new Work();
-        work.setAnswer((List<String>) data.get("answer"));
-        work.setDeclaration(data.get("declaration").toString());
-        work.setWorkName(data.get("workName").toString());
-        work.setTeacherOpenId(data.get("teacherOpenId").toString());
-        work.setStudentOpenId(data.get("studentOpenId").toString());
-        work.setCount((Integer.parseInt(data.get("count").toString())));
-        work.setSelection((List<String[]>) data.get("selection"));
-        work.setTitle((List<String>) data.get("title"));
-        work.setScore(0);
-        workTemplate.save(work);
-        return "success";
-    }
-
-    @ResponseBody
-    @PostMapping("/getWork")
-    public Work getWork(@RequestBody Map<String, Object> data){
-        return workTemplate.findById(new ObjectId(data.get("id").toString()));
-    }
-
-    @ResponseBody
-    @PostMapping("/getAnswer")
-    public Answer getAnswer(@RequestBody Map<String, Object> data){
-        return answerTemplate.findByOpenIdAndWorkId(data.get("openId").toString(), new ObjectId(data.get("workId").toString()));
-    }
-
-    @ResponseBody
-    @PostMapping("/addAnswer")
-    public String addAnswer(@RequestBody Map<String, Object> data) {
-        //TODO: process POST request
-        Answer answer = new Answer();
-        List<String> answerList = (List<String>) data.get("answerList");
-        answer.setAnswer(answerList);
-        answer.setWorkId(new ObjectId(data.get("workId").toString()));
-        answer.setStudentId(data.get("studentOpenId").toString());
-        answerTemplate.saveAnswer(answer);
-        Work work = workTemplate.findById(new ObjectId(data.get("workId").toString()));
-        int tmp = 0;
-        for (int i = 0; i< work.getAnswer().size(); i++){
-            if(work.getAnswer().get(i).equals(answerList.get(i))) tmp++;
-        }
-        System.out.println(tmp);
-        work.setScore(((float)tmp / (float)work.getCount())*100);
-        workTemplate.save(work);
-        return "Success";
-    }
-    
-
+    System.out.println(tmp);
+    work.setScore(((float) tmp / (float) work.getCount()) * 100);
+    workTemplate.save(work);
+    return "Success";
+  }
 }
--- src/main/java/com/gowithu/springboot/entity/Work.java
+++ src/main/java/com/gowithu/springboot/entity/Work.java
@@ -1,121 +1,139 @@
 package com.gowithu.springboot.entity;
 
+import java.util.List;
+import jdk.nashorn.internal.ir.debug.PrintVisitor;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.List;
 
 /**
  * @author david
  */
 @Document(collection = "work")
 public class Work {
-    private ObjectId id;
-    private String stringId;
-    private String workName;
-    private String declaration;
-    private String teacherOpenId;
-    private String studentOpenId;
-    private String filePath;
-    private int count;
-    private List<String> title;
-    private List<String[]> selection;
-    private List<String> answer;
-    private float score;
+  private ObjectId id;
+  private String stringId;
+  private String workName;
+  private String declaration;
+  private String teacherOpenId;
+  private String studentOpenId;
+  private String filePath;
+  private int count;
+  private List<String> title;
+  private List<String[]> selection;
+  private List<String> answer;
+  private float score;
+  private String context;
+  private int type;
 
-    public float getScore(){
-        return score;
-    }
+  public String getContext() {
+    return context;
+  }
 
-    public void setScore(float s){
-        this.score = s;
-    }
+  public int getType() {
+    return type;
+  }
 
-    public int getCount() {
-        return count;
-    }
+  public void setType(int t) {
+    this.type = t;
+  }
 
-    public void setCount(int count) {
-        this.count = count;
-    }
+  public void setContext(String c) {
+    this.context = c;
+  }
 
-    public ObjectId getId() {
-        return id;
-    }
+  public float getScore() {
+    return score;
+  }
 
-    public String getStringId() {
-        return stringId;
-    }
+  public void setScore(float s) {
+    this.score = s;
+  }
 
-    public void setStringId(String stringId) {
-        this.stringId = stringId;
-    }
+  public int getCount() {
+    return count;
+  }
 
-    public String getWorkName() {
-        return workName;
-    }
+  public void setCount(int count) {
+    this.count = count;
+  }
 
-    public void setWorkName(String workName) {
-        this.workName = workName;
-    }
+  public ObjectId getId() {
+    return id;
+  }
 
-    public String getDeclaration() {
-        return declaration;
-    }
+  public String getStringId() {
+    return stringId;
+  }
 
-    public void setDeclaration(String declaration) {
-        this.declaration = declaration;
-    }
+  public void setStringId(String stringId) {
+    this.stringId = stringId;
+  }
 
-    public String getTeacherOpenId() {
-        return teacherOpenId;
-    }
+  public String getWorkName() {
+    return workName;
+  }
 
-    public void setTeacherOpenId(String teacherOpenId) {
-        this.teacherOpenId = teacherOpenId;
-    }
+  public void setWorkName(String workName) {
+    this.workName = workName;
+  }
 
-    public String getStudentOpenId() {
-        return studentOpenId;
-    }
+  public String getDeclaration() {
+    return declaration;
+  }
 
-    public void setStudentOpenId(String studentOpenId) {
-        this.studentOpenId = studentOpenId;
-    }
+  public void setDeclaration(String declaration) {
+    this.declaration = declaration;
+  }
 
-    public String getFilePath() {
-        return filePath;
-    }
+  public String getTeacherOpenId() {
+    return teacherOpenId;
+  }
 
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
-    }
+  public void setTeacherOpenId(String teacherOpenId) {
+    this.teacherOpenId = teacherOpenId;
+  }
 
-    public List<String> getTitle() {
-        return title;
-    }
+  public String getStudentOpenId() {
+    return studentOpenId;
+  }
 
-    public void setTitle(List<String> title) {
-        this.title = title;
-    }
+  public void setStudentOpenId(String studentOpenId) {
+    this.studentOpenId = studentOpenId;
+  }
 
-    public List<String[]> getSelection() {
-        return selection;
-    }
+  public String getFilePath() {
+    return filePath;
+  }
 
-    public void setSelection(List<String[]> selection) {
-        this.selection = selection;
-    }
+  public void setFilePath(String filePath) {
+    this.filePath = filePath;
+  }
 
-    public List<String> getAnswer() {
-        return answer;
-    }
+  public List<String> getTitle() {
+    return title;
+  }
 
-    public void setAnswer(List<String> answer) {
-        this.answer = answer;
-    }
+  public void setTitle(List<String> title) {
+    this.title = title;
+  }
 
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
+  public List<String[]> getSelection() {
+    return selection;
+  }
+
+  public void setSelection(List<String[]> selection) {
+    this.selection = selection;
+  }
+
+  public List<String> getAnswer() {
+    return answer;
+  }
+
+  public void setAnswer(List<String> answer) {
+    this.answer = answer;
+  }
+
+  public void setId(ObjectId id) {
+    this.id = id;
+  }
 }
